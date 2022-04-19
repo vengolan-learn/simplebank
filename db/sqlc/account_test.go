214196218493269df3677d50283d439a7e8e6ca3
vengolan <vengolan@gmail.com>
2022-04-19 20:02:03 +0530

    Added tests for the remaining Account CRUD functions

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,14 +2,15 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"github.com/vengolan/simplebank/db/util"
 )
 
-func TestCreateAccount(t *testing.T) {
-
+func createRandomAccount(t *testing.T) Account {
 	args := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
@@ -24,5 +25,67 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, ac.ID)
 	require.NotZero(t, ac.CreatedAt)
+	return ac
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func Test_GetAccount(t *testing.T) {
+	createdAccount := createRandomAccount(t)
+	gotAccount, err := testQueries.GetAccount(context.Background(), createdAccount.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, gotAccount)
+
+	require.Equal(t, createdAccount.Owner, gotAccount.Owner)
+	require.Equal(t, createdAccount.Balance, gotAccount.Balance)
+	require.Equal(t, createdAccount.Currency, gotAccount.Currency)
+
+	require.WithinDuration(t, createdAccount.CreatedAt, gotAccount.CreatedAt, time.Second)
+}
+
+func Test_UpdateAccount(t *testing.T) {
+	createdAccount := createRandomAccount(t)
+	args := UpdateAccountParams{ID: createdAccount.ID, Balance: util.RandomMoney()}
+	gotAccount, err := testQueries.UpdateAccount(context.Background(), args)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, gotAccount)
+
+	require.Equal(t, args.Balance, gotAccount.Balance)
+	require.NotEqual(t, createdAccount.Balance, gotAccount.Balance)
+	require.Equal(t, createdAccount.Owner, gotAccount.Owner)
+	require.Equal(t, createdAccount.Currency, gotAccount.Currency)
+	require.WithinDuration(t, createdAccount.CreatedAt, gotAccount.CreatedAt, time.Second)
+
+}
+
+func Test_DeleteAccount(t *testing.T) {
+
+	createdAccount := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), createdAccount.ID)
+	require.NoError(t, err)
+
+	gotAccount, err := testQueries.GetAccount(context.Background(), createdAccount.ID)
+	require.Error(t, err)
+	require.Error(t, err, sql.ErrNoRows)
+	require.Empty(t, gotAccount)
+}
+
+func Test_ListAccount(t *testing.T) {
+
+	arg := ListAccountsParams{
+		Limit:  5,
+		Offset: 0,
+	}
+
+	listAccounts, err := testQueries.ListAccounts(context.Background(), arg)
+	require.NoError(t, err)
+	require.Equal(t, len(listAccounts), 5)
+
+	for _, account := range listAccounts {
+		require.NotEmpty(t, account)
+	}
 
 }
