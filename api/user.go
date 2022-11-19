351b33d9949402d85c2dd429c41139c0eb1f2dac
vengolan <vengolan@gmail.com>
2022-11-19 17:46:18 +0530

    security concern corrected - removed password from CreateUser api response

--- api/user.go
+++ api/user.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
@@ -14,6 +15,14 @@ type createUserRequest struct {
 	Password string `json:"Password" binding:"required,min=6"`
 	Fullname string `json:"full_name" binding:"required"`
 	Email    string `json:"email" binding:"required,email"`
+}
+
+type createUserResponse struct {
+	Username          string    `json:"username" binding:"required,alphanum"`
+	Fullname          string    `json:"full_name" binding:"required"`
+	Email             string    `json:"email" binding:"required,email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
 }
 
 func (server *Server) createUser(ctx *gin.Context) {
@@ -48,5 +57,14 @@ func (server *Server) createUser(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
-	ctx.JSON(http.StatusOK, user)
+
+	res := createUserResponse{
+		Username:          user.Username,
+		Fullname:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt:         user.CreatedAt,
+	}
+
+	ctx.JSON(http.StatusOK, res)
 }
